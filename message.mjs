cb4d45650aef5d5a7b588eee1c69bfae2e2a9888
m4eba <m4eba.0@gmail.com>
2022-05-01 21:42:40 +0200

    Skip lines starting with timestamp and remove embedded timestamp

--- message.mjs
+++ message.mjs
@@ -1,28 +1,26 @@
 const timeReg = /<t:\d*>/g;
 const smilieReg = /<a?:(.*?):\d*>/g;
 
-export function format(msg, maxLength){
+export function format(msg, maxLength) {
   let result = '';
-  console.log('message to format',msg);
+  console.log('message to format', msg);
   let lines = msg.split('\n');
-  for(let i=0;i<lines.length;i++){
+  for (let i = 0; i < lines.length; i++) {
     const l = lines[i].trim();
-    
-    if (l.length==0) continue;
-    if (l.search(timeReg)>-1) {
-      continue;
-    }
-    if (l[0] == '>') {
+
+    if (l.length == 0) continue;
+
+    if (l.startsWith('<t:')) {
       continue;
     }
 
-    result = result + ' ' + l.replaceAll(smilieReg,'$1');
-        
+    result =
+      result + ' ' + l.replaceAll(smilieReg, '$1').replaceAll(timeReg, '');
 
-    if (result.length>maxLength) break;
+    if (result.length > maxLength) break;
   }
-  if (result.length>maxLength) {
-    result = result.substring(0,250).trim();
+  if (result.length > maxLength) {
+    result = result.substring(0, maxLength - 3).trim();
     result += '...';
   }
   return result;
